c207f1d55b88131afdaa9bdb2504dbef133067e6
Tyler Mos <48932523+tyler-Mos@users.noreply.github.com>
2019-09-17 14:17:22 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    std::string output = ((N%2==1) || ((N%2==0)&&(6<=N)&&(N<=20))) ? "Weird": "Not Weird";
+    std::cout << output;
+    return 0;
+}
